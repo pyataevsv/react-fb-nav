dbe7e3198c4349c57317849746aed72111a6ffac
pyataevsv <pyataevsv@gmail.com>
2020-07-08 14:24:26 +0300

    Updates

---
+++ precache-manifest.c3d6f03365ead641aa8e61cc7d8c458f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d909efa9734ede70ae77d815ef5d0a2",
+    "url": "/react-fb-nav/index.html"
+  },
+  {
+    "revision": "ea33ccb70e1c03e3decb",
+    "url": "/react-fb-nav/static/css/main.89596d1b.chunk.css"
+  },
+  {
+    "revision": "f45f5ee1dfcf6700f3f6",
+    "url": "/react-fb-nav/static/js/2.d825a05f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-fb-nav/static/js/2.d825a05f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea33ccb70e1c03e3decb",
+    "url": "/react-fb-nav/static/js/main.e116ec73.chunk.js"
+  },
+  {
+    "revision": "969f4dc7596a43ae10b7",
+    "url": "/react-fb-nav/static/js/runtime-main.1603bf88.js"
+  },
+  {
+    "revision": "e9fd40a70989ed93a960628beaf25b7d",
+    "url": "/react-fb-nav/static/media/bell.e9fd40a7.svg"
+  },
+  {
+    "revision": "681afa2ed6c043ea96751e7b755cc561",
+    "url": "/react-fb-nav/static/media/bolt.681afa2e.svg"
+  },
+  {
+    "revision": "615e14d18e96dc6cb0e028991a60ef80",
+    "url": "/react-fb-nav/static/media/caret.615e14d1.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/react-fb-nav/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "9fdc8d67348585808e061c4755bb68f9",
+    "url": "/react-fb-nav/static/media/messenger.9fdc8d67.svg"
+  },
+  {
+    "revision": "9311af2e3926a973133dc6ebe8e8a9aa",
+    "url": "/react-fb-nav/static/media/plus.9311af2e.svg"
+  }
+]);
